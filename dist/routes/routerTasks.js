d65ee717119b89a39fba54fa79c3073a85a506f0
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-11-12 18:26:29 -0300

    atualizado

--- dist/routes/routerTasks.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { creatTask } from "../controllers/filmes-controllers.js";
-var routertasks = Router();
-routertasks.post("/tasks", creatTask);
-export default routertasks;
--- dist/schemas/filmes-schema.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-export var FilmeSchema = Joi.object({
-    name: Joi.string().required(),
-    description: Joi.string().required(),
-    category: Joi.string().required()
-});
